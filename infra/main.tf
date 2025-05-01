fdc18a0bbd91a4a6e4399a28070a434fcaa88703
“Mark <mark.lambe@bjss.com>
2025-05-01 13:09:20 +0100

    Add required revision_mode argument to azurerm_container_app

--- infra/main.tf
+++ infra/main.tf
@@ -31,6 +31,7 @@ resource "azurerm_container_app" "main" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   container_app_environment_id = azurerm_container_registry.main.id
+  revision_mode       = "Single"
 
   template {
     container {
